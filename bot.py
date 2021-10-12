f354625578e4767e8287f270531cfc66b42cfe04
Theeban Kumaresan <44730386+theebank@users.noreply.github.com>
2021-10-12 15:23:01 -0400

    Added leaderboard and timer commands

--- bot.py
+++ bot.py
@@ -3,23 +3,46 @@ import asyncio
 import os
 import random
 import discord
-from discord import Reaction, Member
+import sqlite3
+from sqlite3 import Error
+from discord import Reaction, Member, Client
 from dotenv import load_dotenv
 from discord.ext import commands
 
 load_dotenv()
-intents = intents=discord.Intents.all()
+intent = discord.Intents().all()
 
 orderstarted = False
 coffeeorder = []
 coffeegetter = ""
-
+members = []
 TOKEN = os.getenv('DISCORD_TOKEN')
 Server = os.getenv('DISCORD_SERVER')
-bot = commands.Bot(command_prefix='!',intents = intents)
+bot = commands.Bot(command_prefix='!',intents = intent)
+
+def grabconncurs():
+    connection = sqlite3.connect('leaderboard.db')
+    cursor = connection.cursor()
+    return connection, cursor
+def initialize():
+    connection,cursor = grabconncurs()
+    cursor.execute("DROP TABLE IF EXISTS users")
+    table1 = """CREATE TABLE users(name TEXT, coffeeruns INTEGER)"""
+    cursor.execute(table1)
+
+    for i in members:
+        cursor.execute("INSERT INTO users VALUES (?,0)",(i,))
+    connection.commit()
+    global errormsg
+    errormsg=""
 
 @bot.event
 async def on_ready():
+    clienti = Client()
+    for guild in bot.guilds:
+        for i in guild.members:
+            members.append(i.name)
+    initialize()
     print('Bot is ready!')
 
 @bot.event
@@ -54,11 +77,20 @@ async def add_order_to_run(ctx,*,arg):
 
 @bot.command(name = 'end',no_pm = True,pass_context = True,help = 'Ends the order')
 async def end_order(ctx):
+    connection,cursor = grabconncurs()
     await coffeegetter.create_dm()
     await coffeegetter.dm_channel.send(f"Looks like you're getting the coffee, here's the order:")
+    #Adding total coffee runs
+    cursor.execute("SELECT coffeeruns FROM users WHERE name = ?", (coffeegetter.name,))
+    results = cursor.fetchall()
+    newres = results[0][0]+1
+    cursor.execute("UPDATE users SET coffeeruns = ? WHERE name = ?", (newres, coffeegetter.name))
+    connection.commit()
+
     for x in coffeeorder:
         string = x[0] + " wants: " + x[1] + '\n'
         await coffeegetter.dm_channel.send(string)
+
 
 @bot.event
 async def on_reaction_add(reaction, user):
@@ -66,7 +98,39 @@ async def on_reaction_add(reaction, user):
         if reaction.emoji == '☕':
             global coffeegetter
             coffeegetter = user
-    else:
-        print('hughuguhgug')
 
+@bot.command(name = 'leaderboard',no_pm =True,pass_context = True, help = 'Shows the coffeerun leaderboard!')
+async def get_leaderboard(ctx):
+    connection,cursor = grabconncurs()
+    cursor.execute("SELECT name,coffeeruns FROM users ORDER BY coffeeruns DESC")
+    userlist = cursor.fetchall()
+    gettertop = userlist[0][0]
+    gettertopc = userlist[0][1]
+    gettertop2 = userlist[1][0]
+    gettertop2c = userlist[1][1]
+    gettertop3 = userlist[2][0]
+    gettertop3c = userlist[2][1]
+
+    await ctx.send(f'@everyone! Here are your top 3 coffeerunners in the server! \n #1) {gettertop} with {gettertopc} runs!\n #2) {gettertop2} with {gettertop2c} runs!\n #3) {gettertop3} with {gettertop3c} runs!')
+@bot.command(name = 'timer')
+async def timer(ctx, seconds):
+    try:
+        secondint = int(seconds)
+        if secondint > 300:
+            await ctx.send("I dont think im allowed to do go above 300 seconds.")
+            raise BaseException
+        if secondint <= 0:
+            await ctx.send("I dont think im allowed to do negatives")
+            raise BaseException
+        message = await ctx.send("Timer: {seconds}")
+        while True:
+            secondint -= 1
+            if secondint == 0:
+                await message.edit(content="Ended!")
+                break
+            await message.edit(content=f"Timer: {secondint}")
+            await asyncio.sleep(1)
+        await ctx.send(f"{ctx.author.mention} Your countdown Has ended!")
+    except ValueError:
+        await ctx.send("Must be a number!")
 bot.run(TOKEN)
